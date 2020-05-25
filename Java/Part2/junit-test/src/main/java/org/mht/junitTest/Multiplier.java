88d7374eb1d5894494418a200e9eed6d7da05dc2
2maj <adjimahamat@gmail.com>
2020-05-25 14:35:17 +0200

    all my courses

---
+++ Java/Part2/junit-test/src/main/java/org/mht/junitTest/Multiplier.java
@@ -0,0 +1,7 @@
+package org.mht.junit-test;
+
+public class Multiplier {
+    public int process(int a, int b){
+        return a*b;
+    }
+}
---
+++ Operating system/tp/cmake/gestion_tableaux.h
@@ -0,0 +1,8 @@
+//
+// Created by moussa_24 on 04/05/2020.
+//
+
+#ifndef CMAKE_GESTION_TABLEAUX_H
+#define CMAKE_GESTION_TABLEAUX_H
+
+#endif //CMAKE_GESTION_TABLEAUX_H
